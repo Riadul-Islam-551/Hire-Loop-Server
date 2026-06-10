ed1162ed9ffe2266b0743f9ec11c1428a3b61941
Riadul Islam <riadul551@gmail.com>
2026-06-10 14:09:40 +0600

    add GET endpoint for retrieving job listings with query support

--- index.js
+++ index.js
@@ -31,6 +31,22 @@ async function run() {
     const db = client.db("hire-loop");
     const jobCollection = db.collection("job");
 
+    app.get("/api/jobs", async (req, res) => {
+      const query = {};
+
+      if (req.query.companyId) {
+        query.companyId = req.query.companyId;
+      }
+      if (req.query.status) {
+        query.status = req.query.status;
+      }
+
+      const cursor = jobCollection.find(query);
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
     app.post("/api/jobs", async (req, res) => {
       const data = req.body;
       const result = await jobCollection.insertOne(data);
