dbc042090a3b52ec6a431dd447a1625d03cf487a
Riadul Islam <riadul551@gmail.com>
2026-06-13 23:51:53 +0600

    add GET endpoint for retrieving a job by ID

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -53,6 +53,16 @@ async function run() {
 
       const cursor = jobCollection.find(query);
       const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
+    app.get("/api/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await jobCollection.findOne(query);
 
       res.send(result);
     });
